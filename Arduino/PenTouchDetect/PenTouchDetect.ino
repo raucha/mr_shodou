dfe6affc7c4f155b40c9bbf23a4af59d95412cf6
shige <shige@hoge>
2019-10-13 17:25:37 +0900

    ペンの音実装

--- Arduino/PenTouchDetect/PenTouchDetect.ino
+++ Arduino/PenTouchDetect/PenTouchDetect.ino
@@ -1,104 +1,114 @@
 /*********************************************************
-This is a library for the MPR121 12-channel Capacitive touch sensor
-Designed specifically to work with the MPR121 Breakout in the Adafruit shop 
+  This is a library for the MPR121 12-channel Capacitive touch sensor
+  Designed specifically to work with the MPR121 Breakout in the Adafruit shop
   ----> https://www.adafruit.com/products/
-These sensors use I2C communicate, at least 2 pins are required 
-to interface
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-Written by Limor Fried/Ladyada for Adafruit Industries.  
-BSD license, all text above must be included in any redistribution
+  These sensors use I2C communicate, at least 2 pins are required
+  to interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
 **********************************************************/
 
 #include <Wire.h>
 #include "Adafruit_MPR121.h"
 
 #ifndef _BV
-#define _BV(bit) (1 << (bit)) 
+#define _BV(bit) (1 << (bit))
 #endif
 
 // You can have up to 4 on one i2c bus but one is enough for testing!
 Adafruit_MPR121 cap = Adafruit_MPR121();
 
-// Keeps track of the last pins touched
-// so we know when buttons are 'released'
-uint16_t lasttouched = 0;
-uint16_t currtouched = 0;
+// uint16_t currtouched = 0;
 
 void setup() {
-  Serial.begin(9600);
-
-  while (!Serial) { // needed to keep leonardo/micro from starting too fast!
+  Serial.begin(1000000);
+  while (!Serial) {  // needed to keep leonardo/micro from starting too fast!
     delay(10);
   }
-  
-  Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
-  
+  Serial.println("Adafruit MPR121 Capacitive Touch sensor test");
+
   // Default address is 0x5A, if tied to 3.3V its 0x5B
   // If tied to SDA its 0x5C and if SCL then 0x5D
   if (!cap.begin(0x5A)) {
     Serial.println("MPR121 not found, check wiring?");
-    while (1);
+    while (1)
+      ;
   }
   Serial.println("MPR121 found!");
 }
 
 void loop() {
-  // Get the currently touched pads
-  currtouched = cap.touched();
-  int penVal = cap.filteredData(6);
-//  Serial.println("penVal: ");Serial.print(penVal); 
+  //  ループ周期まで待機
+  static int prevMillis = 0;
+  int currentMillis = millis();
+  const int PERIOD_MS = 10;
+  while (PERIOD_MS > millis() - prevMillis)
+    ;
+  prevMillis = millis();
 
-//  static int isPenPushed = 0;
+  //  static int isPenPushed = 0;
   static int prevPenVal = 0;
   const int release_th = 10;
 
-  Serial.print("penVal: ");Serial.print(penVal);
-  Serial.print("prevPenVal: ");Serial.print(prevPenVal);
-  Serial.print("penßVal-prevPenVal: ");Serial.print(penVal-prevPenVal);
-  Serial.print("release_th: ");Serial.println(release_th); 
+  // 静電容量センサ 生値の取得
+  // currtouched = cap.touched();
+  int penVal_raw = cap.filteredData(6);
 
-  if(release_th < abs(penVal-prevPenVal)){
-    if(penVal < prevPenVal){
-      Serial.println("pen pushed");
-    }else{
-      Serial.println("pen released");      
-    } 
+  // LPF
+  static float penVal = penVal_raw;
+  static float penVal2 = penVal_raw;
+  float k = 0.99;
+  penVal = k * ((float)penVal) + (1.0 - k) * ((float)penVal_raw);
+  float k2 = 0.8;
+  penVal2 = k2 * ((float)penVal2) + (1.0 - k2) * ((float)penVal_raw);
+
+  const int EVENT_STAY = 0;
+  const int EVENT_PUSH = 1;
+  const int EVENT_RELEASE = 2;
+  int penEvent = EVENT_STAY;
+  if (release_th < abs(penVal2 - penVal)) {
+    if (penVal2 < penVal) {
+      penEvent = EVENT_PUSH;
+    } else {
+      penEvent = EVENT_RELEASE;
+    }
   }
   prevPenVal = penVal;
 
-  
-  for (uint8_t i=0; i<12; i++) {
-    // it if *is* touched and *wasnt* touched before, alert!
-    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
-      Serial.print(i); Serial.println(" touched");
-    }
-    // if it *was* touched and now *isnt*, alert!
-    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
-      Serial.print(i); Serial.println(" released");
-    }
-  }
+  // pythonへのデータ送付用
+  Serial.print("{");
+  Serial.print(" \"state\":");
+  Serial.print(penEvent);
+  Serial.print(", \"val\":");
+  Serial.print(penVal);
+  Serial.println("}");
 
-  // reset our state
-  lasttouched = currtouched;
+  //  // シリアルプロット用
+  //  Serial.print(penVal_raw);
+  //  Serial.print(",");
+  //  Serial.print(penVal2);
+  //  Serial.print(",");
+  //  Serial.println(penVal);
 
   // comment out this line for detailed data from the sensor!
   return;
-  
-  // debugging info, what
-  Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
-  Serial.print("Filt: ");
-  for (uint8_t i=0; i<12; i++) {
-    Serial.print(cap.filteredData(i)); Serial.print("\t");
-  }
-  Serial.println();
-  Serial.print("Base: ");
-  for (uint8_t i=0; i<12; i++) {
-    Serial.print(cap.baselineData(i)); Serial.print("\t");
-  }
-  Serial.println();
-  
+
+  //  // デバッグ用
+  //  Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x");
+  //  Serial.println(cap.touched(), HEX); Serial.print("Filt: "); for (uint8_t
+  //  i=0; i<12; i++) {
+  //    Serial.print(cap.filteredData(i)); Serial.print("\t");
+  //  }
+  //  Serial.println();
+  //  Serial.print("Base: ");
+  //  for (uint8_t i=0; i<12; i++) {
+  //    Serial.print(cap.baselineData(i)); Serial.print("\t");
+  //  }
+  //  Serial.println();
+
   // put a delay so it isn't overwhelming
   delay(10);
 }
---
+++ scripts/play.py
@@ -0,0 +1,136 @@
+# coding: utf-8
+
+import serial
+import json
+import argparse
+import pygame
+from scipy.io import wavfile
+import sys
+import numpy as np
+
+PEN_EVENT_STAY = 0
+PEN_EVENT_PUSH = 1
+PEN_EVENT_RELEASE = 2
+
+
+def speedx(snd_array, factor):
+    """ Speeds up / slows down a sound, by some factor. """
+    indices = np.round(np.arange(0, len(snd_array), factor))
+    indices = indices[indices < len(snd_array)].astype(int)
+    return snd_array[indices]
+
+
+def stretch(snd_array, factor, window_size, h):
+    """ Stretches/shortens a sound, by some factor. """
+    phase = np.zeros(window_size)
+    hanning_window = np.hanning(window_size)
+    result = np.zeros(int(len(snd_array) / factor + window_size))
+
+    for i in np.arange(0, len(snd_array) - (window_size + h), h * factor):
+        i = int(i)
+        # Two potentially overlapping subarrays
+        a1 = snd_array[i:i + window_size]
+        a2 = snd_array[i + h:i + window_size + h]
+
+        # The spectra of these arrays
+        s1 = np.fft.fft(hanning_window * a1)
+        s2 = np.fft.fft(hanning_window * a2)
+
+        # Rephase all frequencies
+        phase = (phase + np.angle(s2 / s1)) % 2 * np.pi
+
+        a2_rephased = np.fft.ifft(np.abs(s2) * np.exp(1j * phase))
+        i2 = int(i / factor)
+        result[i2:i2 + window_size] += hanning_window * a2_rephased.real
+
+    # normalize (16bit)
+    result = ((2**(16 - 4)) * result / result.max())
+
+    return result.astype('int16')
+
+
+def pitchshift(snd_array, n, window_size=2**13, h=2**11):
+    """ Changes the pitch of a sound by ``n`` semitones. """
+    factor = 2**(1.0 * n / 12.0)
+    stretched = stretch(snd_array, 1.0 / factor, window_size, h)
+    return speedx(stretched[window_size:], factor)
+
+
+def parse_arguments():
+    description = ('Use your computer keyboard as a "piano"')
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        '--wav',
+        '-w',
+        metavar='FILE',
+        type=argparse.FileType('r'),
+        default='tudumi.wav',
+        # default='bowl.wav',
+        help='WAV file (default: bowl.wav)')
+    # parser.add_argument('--keyboard',
+    #                     '-k',
+    #                     metavar='FILE',
+    #                     type=argparse.FileType('r'),
+    #                     default='typewriter.kb',
+    #                     help='keyboard file (default: typewriter.kb)')
+    parser.add_argument('--verbose',
+                        '-v',
+                        action='store_true',
+                        help='verbose mode')
+
+    return (parser.parse_args(), parser)
+
+
+def main():
+    (args, parser) = parse_arguments()
+
+    fps, sound = wavfile.read(args.wav.name)
+    sys.stdout.write('Transponding sound file... ')
+    sys.stdout.flush()
+    tones = range(-25, 25)
+    transposed_sounds = [pitchshift(sound, n) for n in tones]
+    print('DONE')
+
+    pygame.mixer.init(fps, -16, 1, 2048)
+    # screen = pygame.display.set_mode((150, 150))
+    sounds = list(map(pygame.sndarray.make_sound, transposed_sounds))
+    # sounds = list(map(pygame.sndarray.make_sound, transposed_sounds))
+
+    ser = serial.Serial('COM5', 1000000, timeout=None)
+    # ser = serial.Serial('/dev/ttyS3', 9600, timeout=None)
+    while True:
+        # event = pygame.event.wait()
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        #     pygame.quit()
+        #     raise KeyboardInterrupt
+
+        data = None
+        line = ser.readline()
+        is_playing = {"push": False, "release": False}
+        try:
+            data = json.loads(line)
+            print(data)
+        except ValueError:
+            continue
+
+        # 音を再生
+        # print(data["state"])
+        if PEN_EVENT_PUSH == data["state"]:
+            sounds[13].play(fade_ms=50)
+            sounds[36].fadeout(50)
+            is_playing["push"] = True
+            is_playing["release"] = False
+        if PEN_EVENT_RELEASE == data["state"]:
+            sounds[36].play(fade_ms=50)
+            sounds[13].fadeout(50)
+            is_playing["release"] = True
+            is_playing["push"] = False
+
+        # print(line)
+
+    ser.close()
+
+
+if __name__ == "__main__":
+    main()
